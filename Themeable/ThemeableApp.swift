a072899988059a9d2a06388b87e551ebf815c9e6
Cas Hoefman <cashoefman@gmail.com>
2025-05-15 12:23:09 -0400

    Initial Commit

---
+++ Themeable/ThemeableApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeableApp.swift
+//  Themeable
+//
+//  Created by Cas Hoefman on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ThemeableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
